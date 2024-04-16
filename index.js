a68be1a2e3aef12ff99dd5757851d77a7d9958ac
Glitch (clammy-chill-gopher) <none>
2024-04-16 16:36:53 +0000

    ✂️🖐 Checkpoint ./index.css:59134124/870 ./index.html:59134124/163 ./index.js:59134124/800

--- index.js
+++ index.js
@@ -44,19 +44,9 @@ window.addEventListener('scroll', function() {
         });
     });
 
-const body = document.body;
-   const switch_mode = document.querySelector('#switch-mode i');
-   // Load mode 
-   let mode = localStorage.getItem('darkmode');
-   if(mode=='true'){
-      body.classList.add('dark');
-      switch_mode.className = "bi bi-brightness-high-fill";
-   }
- 
-   switch_mode.addEventListener('click', () =>{
-      switch_mode.classList.toggle('bi-moon-stars-fill');
-      switch_mode.classList.toggle('bi-brightness-high-fill');
-      let mode = body.classList.toggle('dark');
-      // save mode 
-      localStorage.setItem('darkmode', mode);
-   });
+
+// Thêm sự kiện click vào nút chuyển đổi chế độ
+document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+  // Toggle class 'dark-mode' cho thẻ body
+  document.body.classList.toggle('dark-mode');
+});
