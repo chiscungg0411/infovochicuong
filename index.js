64580b6796bedd0ee30548689641e69889320c23
Glitch (clammy-chill-gopher) <none>
2024-04-18 12:32:05 +0000

    🎽🔮 Checkpoint ./index.css:59134124/2010 ./index.html:59134124/24 ./index.js:59134124/598

--- index.js
+++ index.js
@@ -44,6 +44,22 @@ window.addEventListener('scroll', function() {
         });
     });
 
+window.addEventListener('scroll', function() {
+        var sections = document.querySelectorAll('.theend-section');
+        var windowHeight = window.innerHeight;
+        
+        sections.forEach(function(section) {
+            var position = section.getBoundingClientRect().top;
+            
+            if (position < windowHeight * 0.67) {
+                section.classList.add('visible');
+            } else {
+                section.classList.remove('visible'); // Loại bỏ lớp 'visible' khi phần tử không còn trong vùng nhìn thấy
+            }
+        });
+    });
+
+
 function toggleDarkMode() {
       const body = document.body;
       body.classList.toggle('dark-mode');
